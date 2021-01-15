a8703e60ca90a03b5da6800bd55de74b154c58b6
andrewsilent <36265247+andrewsilent@users.noreply.github.com>
2021-01-15 12:04:09 +0200

    add RangeValidator

--- script6.js
+++ script6.js
@@ -1,49 +1,52 @@
-// class RangeValidator {
-//   constructor(from, to) {
-//     this._from = from;
-//     this._to = to;
-//   }
+class RangeValidator {
+  constructor(from, to) {
+    this._from = from;
+    this._to = to;
+  }
+  set from(v) {
+    this._from = v;
+  }
+  get from() {
+    return this._from;
+  }
+  set to(v) {
+    this._to = v;
+  }
+  get to() {
+    return this._to;
+  }
+  get range() {
+    return Array(this._from, this._to);
+  }
+  validate(n) {
+    return (n >= this._from && n <= this._to)
+  }
+}
 
-//   set from(v) {
-//     this._from = v;
-//   }
-//   get from() {
-//     return this._from;
-//   }
-//   set to(v) {
-//     this._to = v;
-//   }
-//   get to() {
-//     return this._to;
-//   }
+const r = new RangeValidator(20, 24);
 
-//   getRange() {
 
-//   }
 
-//   validate(n) {
 
-//   }
 
+
+// const users = [
+//   {
+//     name: "name surname",
+//   },
+//   {
+//     name: "name2 surname2",
+//   },
+// ];
+
+// for (const user of users) {
+//   console.log(user.name);
 // }
 
-const users = [
-  {
-    name: "name surname",
-  },
-  {
-    name: "name2 surname2",
-  },
-];
-
-for (const user of users) {
-  console.log(user.name);
-}
-
-function sum(a, b, ...numbers) {
-  let result = a + b;
-  for (const number of numbers) {
-    result += number;
-  }
-  return result;
-}
+// function sum(a, b, ...numbers) {
+//   let result = a + b;
+//   for (const number of numbers) {
+//     result += number;
+//   }
+//   return result;
+// }
